f69481f9b6fa6737c17999f4b3e10b75bb76dfa1
Linh Ngo <lngo@wcupa.edu>
2019-11-29 23:00:13 -0500

    update and modify with Jupyter notebook

--- docker/spark-submit/spark-submit.sh
+++ docker/spark-submit/spark-submit.sh
@@ -1,10 +1,12 @@
  #!/bin/bash
- 
-/spark/bin/spark-submit \
---class ${SPARK_APPLICATION_MAIN_CLASS} \
---master ${SPARK_MASTER_URL} \
---deploy-mode cluster \
---total-executor-cores 1 \
- ${SPARK_SUBMIT_ARGS} \
- ${SPARK_APPLICATION_JAR_LOCATION} \
- ${SPARK_APPLICATION_ARGS} \
+
+jupyter notebook --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 --allow-root
+
+#/spark/bin/spark-submit \
+#--class ${SPARK_APPLICATION_MAIN_CLASS} \
+#--master ${SPARK_MASTER_URL} \
+#--deploy-mode cluster \
+#--total-executor-cores 1 \
+# ${SPARK_SUBMIT_ARGS} \
+# ${SPARK_APPLICATION_JAR_LOCATION} \
+# ${SPARK_APPLICATION_ARGS} \
