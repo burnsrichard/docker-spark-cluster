c914083f371ce44826a2f669d6b1a54414691f25
Linh B Ngo <lngo@wcupa.edu>
2019-12-10 10:07:37 -0500

    minor update,

--- build-images.sh
+++ build-images.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+docker image rm spark-submit
+docker image rm spark-worker
+docker image rm spark-master
+docker image rm spark-base
+
 docker build -t spark-base:latest ./docker/base
 docker build -t spark-master:latest ./docker/spark-master
 docker build -t spark-worker:latest ./docker/spark-worker
--- docker/spark-master/start-master.sh
+++ docker/spark-master/start-master.sh
@@ -12,4 +12,6 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
+jupyter notebook --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 --allow-root &
+
 cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
